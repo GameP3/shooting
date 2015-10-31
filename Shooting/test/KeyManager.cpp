1567357ab466420c8190d9fff7bbf7f72a0998ba
potter77 <hasimotofu@gmail.com>
2015-10-31 13:46:56 +0900

    操作可能キー追加

--- Shooting/test/KeyManager.cpp
+++ Shooting/test/KeyManager.cpp
@@ -9,6 +9,39 @@ KeyManager::KeyManager()
 	m_down_button = FREE;
 	m_left_button = FREE;
 	m_right_button = FREE;
+	m_a_button = FREE;
+	m_b_button = FREE;
+	m_c_button = FREE;
+	m_d_button = FREE;
+	m_e_button = FREE;
+	m_f_button = FREE;
+	m_g_button = FREE;
+	m_h_button = FREE;
+	m_i_button = FREE;
+	m_j_button = FREE;
+	m_k_button = FREE;
+	m_l_button = FREE;
+	m_m_button = FREE;
+	m_n_button = FREE;
+	m_o_button = FREE;
+	m_p_button = FREE;
+	m_q_button = FREE;
+	m_r_button = FREE;
+	m_s_button = FREE;
+	m_t_button = FREE;
+	m_u_button = FREE;
+	m_v_button = FREE;
+	m_w_button = FREE;
+	m_x_button = FREE;
+	m_y_button = FREE;
+	m_z_button = FREE;
+	m_back_button = FREE; //バックスペースキー 
+	m_tab_button = FREE; // タブキー 
+	m_return_button = FREE; // エンターキー
+	m_lshift_button = FREE; // 左シフトキー
+	m_rshift_button = FREE; // 右シフトキー
+	m_escape_button = FREE; // エスケープキー
+	m_space_button = FREE; // スペースキー
 }
 
 void KeyManager::KeyCheck()	//キーの状態をチェック
@@ -17,6 +50,40 @@ void KeyManager::KeyCheck()	//キーの状態をチェック
 	KeyUpData(&m_down_button, CheckHitKey(KEY_INPUT_DOWN));
 	KeyUpData(&m_left_button, CheckHitKey(KEY_INPUT_LEFT));
 	KeyUpData(&m_right_button, CheckHitKey(KEY_INPUT_RIGHT));
+	KeyUpData(&m_a_button, CheckHitKey(KEY_INPUT_A));
+	KeyUpData(&m_b_button, CheckHitKey(KEY_INPUT_B));
+	KeyUpData(&m_c_button, CheckHitKey(KEY_INPUT_C));
+	KeyUpData(&m_d_button, CheckHitKey(KEY_INPUT_D));
+	KeyUpData(&m_e_button, CheckHitKey(KEY_INPUT_E));
+	KeyUpData(&m_f_button, CheckHitKey(KEY_INPUT_F));
+	KeyUpData(&m_g_button, CheckHitKey(KEY_INPUT_G));
+	KeyUpData(&m_h_button, CheckHitKey(KEY_INPUT_H));
+	KeyUpData(&m_i_button, CheckHitKey(KEY_INPUT_I));
+	KeyUpData(&m_j_button, CheckHitKey(KEY_INPUT_J));
+	KeyUpData(&m_k_button, CheckHitKey(KEY_INPUT_K));
+	KeyUpData(&m_l_button, CheckHitKey(KEY_INPUT_L));
+	KeyUpData(&m_m_button, CheckHitKey(KEY_INPUT_M));
+	KeyUpData(&m_n_button, CheckHitKey(KEY_INPUT_N));
+	KeyUpData(&m_o_button, CheckHitKey(KEY_INPUT_O));
+	KeyUpData(&m_p_button, CheckHitKey(KEY_INPUT_P));
+	KeyUpData(&m_q_button, CheckHitKey(KEY_INPUT_Q));
+	KeyUpData(&m_r_button, CheckHitKey(KEY_INPUT_R));
+	KeyUpData(&m_s_button, CheckHitKey(KEY_INPUT_S));
+	KeyUpData(&m_t_button, CheckHitKey(KEY_INPUT_T));
+	KeyUpData(&m_u_button, CheckHitKey(KEY_INPUT_U));
+	KeyUpData(&m_v_button, CheckHitKey(KEY_INPUT_V));
+	KeyUpData(&m_w_button, CheckHitKey(KEY_INPUT_W));
+	KeyUpData(&m_x_button, CheckHitKey(KEY_INPUT_X));
+	KeyUpData(&m_y_button, CheckHitKey(KEY_INPUT_Y));
+	KeyUpData(&m_z_button, CheckHitKey(KEY_INPUT_Z));
+	KeyUpData(&m_back_button, CheckHitKey(KEY_INPUT_BACK));
+	KeyUpData(&m_tab_button, CheckHitKey(KEY_INPUT_TAB));
+	KeyUpData(&m_return_button, CheckHitKey(KEY_INPUT_RETURN));
+	KeyUpData(&m_lshift_button, CheckHitKey(KEY_INPUT_LSHIFT));
+	KeyUpData(&m_rshift_button, CheckHitKey(KEY_INPUT_RSHIFT));
+	KeyUpData(&m_escape_button, CheckHitKey(KEY_INPUT_ESCAPE));
+	KeyUpData(&m_space_button, CheckHitKey(KEY_INPUT_SPACE));
+
 }
 
 void KeyManager::KeyUpData(int *key_data,int w_key)
--- Shooting/test/KeyManager.h
+++ Shooting/test/KeyManager.h
@@ -21,6 +21,39 @@ public:
 	int get_down_button() { return m_down_button; }
 	int get_right_button() { return m_right_button; }
 	int get_left_button() { return m_left_button; }
+	int get_a_button() { return m_a_button; }
+	int get_b_button() { return m_b_button; }
+	int get_c_button() { return m_c_button; }
+	int get_d_button() { return m_d_button; }
+	int get_e_button() { return m_e_button; }
+	int get_f_button() { return m_f_button; }
+	int get_g_button() { return m_g_button; }
+	int get_h_button() { return m_h_button; }
+	int get_i_button() { return m_i_button; }
+	int get_j_button() { return m_j_button; }
+	int get_k_button() { return m_k_button; }
+	int get_l_button() { return m_l_button; }
+	int get_m_button() { return m_m_button; }
+	int get_n_button() { return m_n_button; }
+	int get_o_button() { return m_o_button; }
+	int get_p_button() { return m_p_button; }
+	int get_q_button() { return m_q_button; }
+	int get_r_button() { return m_r_button; }
+	int get_s_button() { return m_s_button; }
+	int get_t_button() { return m_t_button; }
+	int get_u_button() { return m_u_button; }
+	int get_v_button() { return m_v_button; }
+	int get_w_button() { return m_w_button; }
+	int get_x_button() { return m_x_button; }
+	int get_y_button() { return m_y_button; }
+	int get_z_button() { return m_z_button; }
+	int get_back_button() { return m_back_button; }
+	int get_tab_button() { return m_tab_button; }
+	int get_return_button() { return m_return_button; }
+	int get_lshift_button() { return m_lshift_button; }
+	int get_rshift_button() { return m_rshift_button; }
+	int get_escape_button() { return m_escape_button; }
+	int get_space_button() { return m_space_button; }
 
 private:
 	//ボタンの読み込み！
@@ -30,4 +63,37 @@ private:
 	int m_down_button;
 	int m_right_button;
 	int m_left_button;
+	int m_a_button;
+	int m_b_button;
+	int m_c_button;
+	int m_d_button;
+	int m_e_button;
+	int m_f_button;
+	int m_g_button;
+	int m_h_button;
+	int m_i_button;
+	int m_j_button;
+	int m_k_button;
+	int m_l_button;
+	int m_m_button;
+	int m_n_button;
+	int m_o_button;
+	int m_p_button;
+	int m_q_button;
+	int m_r_button;
+	int m_s_button;
+	int m_t_button;
+	int m_u_button;
+	int m_v_button;
+	int m_w_button;
+	int m_x_button;
+	int m_y_button;
+	int m_z_button;
+	int m_back_button; //バックスペースキー 
+	int m_tab_button; // タブキー
+	int m_return_button; // エンターキー
+	int m_lshift_button; // 左シフトキー
+	int m_rshift_button; // 右シフトキー
+	int m_escape_button; // エスケープキー
+	int m_space_button; // スペースキー
 };
